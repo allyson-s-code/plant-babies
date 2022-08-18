22749349faaa6fe7d50b7a7a3753d1c081c236c0
allyson_s <allysonsmithstudio@gmail.com>
2022-08-17 20:43:19 -0400

    Initial commit

---
+++ src/components/list.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function List() {
+  return (
+    <ul>
+      <li>
+        <div className="flex">
+          <img src="" alt="img" />
+          <div className="name"></div>
+        </div>
+      </li>
+    </ul>
+  );
+}
